d46cfb0afdb2a8e239fe229d76798f056e7b6c1d
Todor Belchev <71307782+TodorBelchev@users.noreply.github.com>
2025-03-28 18:21:30 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,6 +5,13 @@ terraform {
       version = "4.25.0"
     }
   }
+
+  backend "azurerm" {
+    resource_group_name  = "StorageRG"
+    storage_account_name = "taskboardsgtodorbelchev"
+    container_name       = "taskboardcontainer"
+    key                  = "terraform.tfstate"
+  }
 }
 
 provider "azurerm" {
